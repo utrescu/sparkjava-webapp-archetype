99ad2cab7985c3ad249d92c7f5b41654e017a0a4
Xavier Sala <utrescu@gmail.com>
2015-02-20 16:36:16 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,17 @@
+package $package;
+
+import static spark.Spark.get;
+import spark.servlet.SparkApplication;
+
+public class App implements SparkApplication {
+
+  @Override
+  public void init() {    
+
+    get("/", (req, res) -> {
+      return "Hello, world";
+    });
+    
+  }
+
+}
